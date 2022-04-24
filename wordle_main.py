ad0eca262c8d9d032333457919d5fba4cc946a44
pontusintheair <58473014+pontusintheair@users.noreply.github.com>
2022-04-24 23:29:56 +0200

    T7

--- wordle_main.py
+++ wordle_main.py
@@ -1,26 +1,24 @@
-import json
-import os, sys
 import random
 import re
 
-def solve_for_words():
-    valid_words = wordlist
-    while True:
-        guess = make_guess(valid_words)
-        print("Guess: " + guess())
-        result = collect_result()
-        if result == correct:
-            print("I won!")
-            break
-    valid_words= update_valid_words(valid_words, guess, result)
+def solve():
+  valid_words = ALL_WORDS
+  while True:
+    guess = make_guess(valid_words)
+    print("Guess: " + guess.upper())
+    result = collect_result()
+    if result == CORRECT:
+        print("I won!")
+        break
+    valid_words = update_valid_words(valid_words, guess, result)
 
 def make_guess(valid_words):
     return random.choice(valid_words)
 
 def collect_result():
   # Collect the result of our guess from the user
-  result = input("What's the result? (_/1/2) ")
-  match = re.match(r'^[21_]{5}$', result)
+  result = input("What's the result? (_/?/!) ")
+  match = re.match(r'^[!?_]{5}$', result)
   if not match:
     print("Invalid response string, try again")
     return collect_result()
@@ -31,11 +29,11 @@ def get_result(guess, answer):
   result = ""
   for pos, ch_guess, ch_answer in zip(range(5), guess, answer):
     if ch_guess == ch_answer:
-      result += "2"
+      result += "!"
     elif ch_guess not in answer:
       result += "_"
     else:
-      result += "1"
+      result += "?"
   return result
   
 def update_valid_words(valid_words, guess, result):
